1d0f5662bcc225722a4874cae8ccb158bfcfad30
Austin Emser <austin.emser@gmail.com>
2017-07-20 15:23:57 -0400

    Finish exercise 1

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -35,15 +35,11 @@ namespace Vidly.Controllers
         }
 
         // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+            var movies = GetMovies();
+            
+            return View(movies);
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
@@ -51,6 +47,15 @@ namespace Vidly.Controllers
         {
 
             return Content(year + "/" + month);
+        }
+
+        private List<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() { Id=1, Name="Shrek" },
+                new Movie() { Id=2, Name="Wall-e" }
+            };
         }
     }
 }
