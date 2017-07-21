d3e5d904165a14565a7cfe01858d0d639b30a192
Austin Emser <austin.emser@gmail.com>
2017-07-21 15:27:51 -0400

    Switch to camelcasing

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -19,7 +19,6 @@ namespace Vidly.Dtos
         
         public byte MembershipTypeId { get; set; }
         
-        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
