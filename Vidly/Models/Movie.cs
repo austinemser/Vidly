e8d1046a511150839b5cb1dc166eae61beea3d83
Austin Emser <austin.emser@gmail.com>
2017-07-21 12:25:43 -0400

    Fix the movies foreign key

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +10,23 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
         public int GenreId { get; set; }
-        public DateTime? ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
     }
 }
