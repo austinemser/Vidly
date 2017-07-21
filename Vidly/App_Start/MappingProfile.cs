a46276ea9ec0183517c078debc15778c26f458da
Austin Emser <austin.emser@gmail.com>
2017-07-21 16:45:05 -0400

    Add api to movies

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -15,6 +15,10 @@ namespace Vidly.App_Start
             CreateMap<Customer, CustomerDto>();
             CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
             CreateMap<MembershipType, MembershipTypeDto>();
+
+            CreateMap<Movie, MovieDto>();
+            CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
+            CreateMap<Genre, GenreDto>();
         }
         
     }
