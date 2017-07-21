ec7451d9abc4a30c06edbfa422842e15ca4e0673
Austin Emser <austin.emser@gmail.com>
2017-07-21 11:16:30 -0400

    Add save functionality

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -34,9 +34,16 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if(customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+            }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
